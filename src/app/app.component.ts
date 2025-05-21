9d8cf9481a308bced865ef8959a05d57e8ff0fdf
ttariik <tarik.sabanovic2@icloud.com>
2025-05-21 21:50:04 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,39 +1,27 @@
 import { Component } from '@angular/core';
-import {ErrorStateMatcher} from '@angular/material/core';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {
   FormControl,
-  FormGroupDirective,
-  NgForm,
   Validators,
-  FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatFormFieldModule,
-    FormsModule, 
-    MatFormFieldModule, 
-   ReactiveFormsModule
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
-
 export class LoginComponent {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-
-  matcher = new MyErrorStateMatcher();
 
 }
