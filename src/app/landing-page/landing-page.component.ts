f1df8e6117ef746967e0f38398672f6b48fa7284
Ejran <ejrannahimzadah@gmail.com>
2025-05-22 20:31:56 +0200

    login in landingpage

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
