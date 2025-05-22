0fb77604cea3ea8184b6222c132d9683046c7c9e
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-05-22 19:17:21 +0200

    feat: Firestore and Authentication

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'DA-Bubble' title`, () => {
+  it(`should have the 'dabubble' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DA-Bubble');
+    expect(app.title).toEqual('dabubble');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, DA-Bubble');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, dabubble'
+    );
   });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "dabubble-ebf4f", appId: "1:50180363425:web:fbd792805e7d8eeb7d0ce0", storageBucket: "dabubble-ebf4f.firebasestorage.app", apiKey: "AIzaSyBbN-1g0WcRxkfn6GcUa9qlzHkXnA3kB8A", authDomain: "dabubble-ebf4f.firebaseapp.com", messagingSenderId: "50180363425" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
