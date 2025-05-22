bf1737cba709caabc2f16f058939cf1d00eb74a8
ttariik <tarik.sabanovic2@icloud.com>
2025-05-22 17:51:59 +0200

    refactor: remove Angular Material from LoginComponent and implement custom form UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {
-  FormControl,
-  FormGroup,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     CommonModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatProgressSpinnerModule,
-    ReactiveFormsModule,
-    MatIconModule
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
@@ -32,7 +16,6 @@ import { MatIconModule } from '@angular/material/icon';
 export class LoginComponent {
   isLoading = false;
   hidePassword = true;
-
 
   emailFormControl = new FormControl('', [
     Validators.required,
@@ -49,23 +32,23 @@ export class LoginComponent {
     password: this.passwordFormControl,
     rememberMe: new FormControl(false)
   });
-  
 
   onSubmit() {
     if (this.loginForm.valid) {
       this.isLoading = true;
       const { email, password } = this.loginForm.value;
+      
+      // Fake loading
       setTimeout(() => {
         console.log('Login with', email, password);
         this.isLoading = false;
-      }, 1500); 
+      }, 1500);
     }
   }
 
-
-togglePasswordVisibility(): void {
-  this.hidePassword = !this.hidePassword;
-}
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
 
   get email() {
     return this.emailFormControl;
