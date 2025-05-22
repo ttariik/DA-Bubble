3e102f9a57e546d3ee4f3b9b07b0b9ebe1dd11d2
Ejran <ejrannahimzadah@gmail.com>
2025-05-22 23:33:43 +0200

    Avatar popup design

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from "./login/login.component";
+import { AvatarComponent } from "./avatar/avatar.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LoginComponent],
+  imports: [
+    LoginComponent, 
+    // AvatarComponent
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
