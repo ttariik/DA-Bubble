84e007de12e4c557710de64655ad4b59868552be
Ejran <ejrannahimzadah@gmail.com>
2025-05-23 18:17:27 +0200

    create profile-component

---
+++ src/app/profile-information/profile-information.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-information',
+  imports: [],
+  templateUrl: './profile-information.component.html',
+  styleUrl: './profile-information.component.scss'
+})
+export class ProfileInformationComponent {
+
+}
