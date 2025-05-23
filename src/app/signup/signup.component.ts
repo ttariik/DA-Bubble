a78fb2dcd761e529ff6e4a78c6a13311030ed266
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-05-23 19:31:39 +0200

    feat: add signUp component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,11 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FooterComponent, HeaderComponent } from '../shared';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HeaderComponent,
+    RouterModule,
+    FooterComponent,
+  ],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  authService = inject(AuthService);
+  private router = inject(Router);
+  isLoading = false;
+  hidePassword = true;
 
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+
+  privacyPolicyFormControl = new FormControl(false, [Validators.required]);
+
+  registerForm = new FormGroup({
+    name: this.nameFormControl,
+    email: this.emailFormControl,
+    password: this.passwordFormControl,
+    privacyPolicy: this.privacyPolicyFormControl,
+  });
+
+  async onSubmit() {
+    if (this.registerForm.valid) {
+      this.isLoading = true;
+      const { email, password } = this.registerForm.value;
+
+      if (email != null && password != null) {
+        await this.authService
+          .signIn(email, password)
+          .then((user: any) => {
+            this.isLoading = false;
+            console.log(user);
+          })
+          .catch((errorCode) => {
+            this.isLoading = false;
+            console.warn(errorCode);
+          });
+      }
+    }
+  }
+
+  get name() {
+    return this.nameFormControl;
+  }
+
+  get email() {
+    return this.emailFormControl;
+  }
+
+  get password() {
+    return this.passwordFormControl;
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  navigateToForgotPassword(): void {
+    this.router.navigate(['/forgot-password']);
+  }
 }
